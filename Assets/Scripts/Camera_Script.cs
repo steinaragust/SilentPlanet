fdbbc609342ea71e1c4b2fb96a78665d9cdbd728
kotkarl <kotkarl@gmail.com>
2015-05-03 20:35:13 +0000

    simplified graphics

--- Assets/Scripts/Camera_Script.cs
+++ Assets/Scripts/Camera_Script.cs
@@ -12,6 +12,6 @@ public class Camera_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(the_player.position.x + 1, the_player.position.y + 1.5f, -20); 
+		transform.position = new Vector3(the_player.position.x, the_player.position.y + 1, -20); 
 	}
 }
