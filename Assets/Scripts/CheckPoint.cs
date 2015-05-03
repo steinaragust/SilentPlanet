e8e5e92e7dd049c6d769a36539355c058e0e8af2
steinaragust <steinar13@ru.is>
2015-05-03 19:02:12 +0000

    checkpoints & respawn

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class CheckPoint : MonoBehaviour {
 
+	public LevelManager levelManager;
+	
 	// Use this for initialization
 	void Start () {
-	
+		levelManager = FindObjectOfType<LevelManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.name == "Player_Bird") {
+			levelManager.currentCheckpoint = gameObject;
+			Debug.Log("Activated Checkpoint! " + transform.position);
+		}
 	}
 }
