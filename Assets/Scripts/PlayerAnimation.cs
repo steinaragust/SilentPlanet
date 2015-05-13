b96cc85b2de651b9d8d8f5d5659638b919f2fb70
kotkarl <kotkarl@gmail.com>
2015-05-13 14:14:49 +0000

    added some animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	// Variables
+	public Animator anim; // Refrerence to the animator
+	//private float fallSpeed; // The speed the character falls
+	//private float verticalMovement; // The amount of vertical movement
+	//private bool onGround; // Flag to check whether the character is on the ground
+	private Player_Script player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player_Bird").GetComponent<Player_Script> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.move != 0) {
+			anim.SetBool ("Running", true);
+		} else {
+			anim.SetBool ("Running", false);
+		}
+
+	}
+}
