e0479da77a9c0f5255ad44921ec126bd6987b330
steinaragust <steinar13@ru.is>
2015-05-14 23:11:33 +0000

    animation on flying enemy

--- Assets/Scripts/FlyingEnemyPatrol.cs
+++ Assets/Scripts/FlyingEnemyPatrol.cs
@@ -14,28 +14,52 @@ public class FlyingEnemyPatrol : MonoBehaviour {
 	public bool stunned;
 
 	public float howLongStunned;
-	private float stunnedFor;
+	public float stunnedFor;
 
 	public float turnAroundTime;
 	public float countdown;
 
 	public float howMuchScale;
 
+	public Animator enemyAnimator;
+
+	public int number;
+
 
 	// Use this for initialization
 	void Start () {
 		stunned = false;
 		countdown = turnAroundTime;
+		enemyAnimator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (stunnedFor > 0) {
+//		if (moveRight && !stunned) {//FlyingRight
+//			enemyAnimator.SetInteger ("State", 0);
+//			number = 0;
+//		} 
+//		if (stunned) {//IdleRight
+//			enemyAnimator.SetInteger ("State", 1);
+//			number = 1;
+//		}
+//		else if (!moveRight && !stunned) {//FlyingLeft
+//			enemyAnimator.SetInteger ("State", 2);
+//			number = 2;
+//		}
+//		else {//IdleLeft
+//			enemyAnimator.SetInteger ("State", 3);
+//			number = 3;
+//		}
+		
+		if (stunnedFor >= 0) {
 			stunnedFor -= Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, 0f);
 			return;
 		} 
 		else {
 			stunned = false;
+			enemyAnimator.SetInteger ("State", 0);
 		}
 		hittingWall = Physics2D.OverlapCircle (wallCheck.position, wallCheckRadius, WhatIsWall);
 		if (countdown <= 0 || hittingWall) {
@@ -61,6 +85,8 @@ public class FlyingEnemyPatrol : MonoBehaviour {
 		if (other.tag == "TrapsForEnemys") {
 			stunned = true;
 			stunnedFor = howLongStunned;
+			enemyAnimator.SetInteger ("State", 1);
+			number = 1;
 		}
 	}
 }
