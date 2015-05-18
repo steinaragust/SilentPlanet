ac4f99af72268e1acb776fbed0895e19e2557cbe
steinaragust <steinar13@ru.is>
2015-05-18 22:05:08 +0000

    damn it

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sign : MonoBehaviour {
+
+	public ToolTipController render;
+	public string theMessage;
+
+	// Use this for initialization
+	void Start () {
+//		render = FindObjectOfType<ToolTipController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name == "Player_Bird"){
+			render.Entering(theMessage);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.name == "Player_Bird"){
+			render.Exiting();
+		}
+	}
+}
---
+++ Assets/Scripts/ToolTipController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToolTipController : MonoBehaviour {
+//	public bool isActive;
+	
+	public GameObject toolTipCanvas;
+	public Text text;
+
+	void Start() {
+//		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (isPaused) {
+//			pauseMenuCanvas.SetActive(true);
+//			Time.timeScale = 0f;
+//		} 
+//		else {
+//			pauseMenuCanvas.SetActive(false);
+//			Time.timeScale = 1f;
+//		}
+//		if (Input.GetKeyDown (KeyCode.Escape)) {
+//			isPaused = !isPaused;
+//		}
+	}
+	public void Entering(string theMessage){
+		text.text = theMessage;
+		toolTipCanvas.SetActive (true);
+	}
+
+	public void Exiting(){
+		toolTipCanvas.SetActive (false);
+	}
+}
